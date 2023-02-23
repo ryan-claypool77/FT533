acd58d7ff0bd806322622e0df3a52400fe42201a
Ryan Claypool <rmc82@duke.edu>
2023-02-23 15:02:45 -0800

    organizing sections of the code by class, functions, main

--- Homework/HW2/hw2.py
+++ Homework/HW2/hw2.py
@@ -7,6 +7,7 @@ n2: integer, default = 5
 asset: string, default = 'IVV'
 
 Task: Create an interactive backtest in Dash for a simple strategy
+
 Strategy:
 1. Before market open each morning, submit a limit order to buy asset at
     Entry Price = (1 + alpha1) * Yesterday Close
@@ -25,7 +26,16 @@ Design Notes:
 """
 import pandas as pd
 
+# Redesign for Refinitiv data creating a csv file for the user's requested stock
+asset = 'IVV'  # in the future, this needs to be fed in as a parameter from Refinitiv
 stock_data = pd.read_csv('IVV.csv')
+
+# trading parameters for buy and sell orders
+alpha1 = -.01  # buying -1% below yest. close
+n1 = 3  # days buy order is open
+alpha2 = .01  # selling +1% above entry_price
+n2 = 5  # days sell order is open
+
 
 # sandbox for playing around with DataFrames in Pandas https://www.dataquest.io/blog/tutorial-indexing-dataframes-in-pandas/#:~:text=Essentially%2C%20there%20are%20two%20main,different%20types%20of%20dataframe%20indexing.
 # print(stock_data)
@@ -33,29 +43,42 @@ stock_data = pd.read_csv('IVV.csv')
 
 
 class Order:
-    def __init__(self):
-        self.alpha1 = -.01
-        self.n1 = 3
-        self.alpha2 = .01
-        self.n2 = 5
-        self.asset = 'IVV'  # in the future, this needs to be fed in as a parameter from Refinitiv
-        self.data = pd.read_csv('IVV.csv')
+    trade_id = 0
 
-    def buy_strategy(self, entry_price):
-        pass
-        # 1. Create a loop that indexes over n1 trading days (logic for weekends?),
-        # 2. Two Paths:
-        #    i. Cancel
-        #    ii. Log the buy in the blotter and call sell_strategy(entry_price) immediately
+    def __init__(self, date, trip, action, order_type):
+        Order.trade_id += 1
+        self.date = date
+        self.asset = asset
+        self.trip = trip  # Enter/Exit
+        self.action = action
+        self.type = order_type
+        self.price = buy_price(stock_data[['Close']])
 
-    def sell_strategy(self, entry_price):
-        pass
-        # 1. Create a loop that indexes over n2 trading days (logic for weekends?),
-        # 2. Two Paths:
-        #    i. Fill the sell order and log sale on blotter.
-        #    ii. Exit the order if not filled by the time the market is about to close at the end of the
-        #        (n2)th trading day, immediately post a market order sale to blotter at closing price.
+        # use Unittest to check an Order before expanding
+
+
+def buy_price(close):
+    result = close
+    return result  # placeholder
+    # 1. Create a loop that indexes over n1 trading days (logic for weekends?), by trade ID
+    # 2. Two Paths:
+    #    i.  Cancel
+    #    ii. Log the buy in the blotter and call sell_strategy(entry_price) immediately
+
+
+def sell_price(entry_price):
+    result = entry_price  # placeholder
+    return result  # should return sell price
+    # 1. Create a loop that indexes over n2 trading days (logic for weekends?),
+    # 2. Two Paths:
+    #    i.  Fill the sell order and log sale on blotter.
+    #    ii. Exit the order if not filled by the time the market is about to close at the end of the
+    #        (n2)th trading day, immediately post a market order sale to blotter at closing price.
 
 
 if __name__ == "__main__":
     pass
+
+    # 1. Sort csv by date (maybe in the initialization?)
+    # 2. While-loop the Pandas dataframe
+    # 3. For each day on the csv, create an Order object that fills the parameters in the blotter table
