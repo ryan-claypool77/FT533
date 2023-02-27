53416ae04a61fde0edd03749397a203edc9f1ed4
julsleo <87832849+julsleo@users.noreply.github.com>
2023-02-26 22:02:29 -0500

    Update hw2_jl.py

--- Homework/HW2/hw2_jl.py
+++ Homework/HW2/hw2_jl.py
@@ -99,7 +99,7 @@ controls = dbc.Card(
     body=True
 )
 
-image = 'FT533/Homework/HW2/ss_react_graph.png'
+image = 'Homework/HW2/ss_react_graph.png'
 test_base64 = base64.b64encode(open(image, 'rb').read()).decode('ascii')
 img_card = dbc.Card(
     [
